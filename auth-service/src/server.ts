71d23983300dece63f433d84444fd63cd98568cc
PhanTruc234 <phantruc313@gmail.com>
2026-05-30 16:02:15 +0700

    setup

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
-
+import cookieParser from "cookie-parser";
+import "./actions/auth.hemera";
+import authRoute from "./routes/auth.route"
+import { errorHandler } from './middlewares/error.middleware';
 dotenv.config();
 
 const app = express();
@@ -10,9 +13,14 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(helmet());
-
+app.use("/api/auths", authRoute)
+app.use(errorHandler);
 app.listen(PORT, () => {
     console.log(`🚀 Order Service running on port ${PORT}`);
 });
--- product-service/src/server.ts
+++ product-service/src/server.ts
@@ -2,17 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser"
+import { errorHandler } from './middlewares/error.middleware';
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(helmet());
-
+app.use(errorHandler);
 app.listen(PORT, () => {
     console.log(`🚀 Order Service running on port ${PORT}`);
 });
