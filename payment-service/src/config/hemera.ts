25d6d58bf4dff33fb7cdf744718694f958e7b332
PhanTruc234 <phantruc313@gmail.com>
2026-05-30 09:53:55 +0700

    setup hemera

--- payment-service/src/config/hemera.ts
+++ payment-service/src/config/hemera.ts
@@ -1,9 +1,12 @@
-import Hemera from 'hemera'
+import Hemera from 'nats-hemera'
 import nats from 'nats'
 
 const nc = nats.connect({
     servers: ['nats://nats:4222']
 })
 
-const hemera = new Hemera(nc)
+const hemera = new Hemera(nc, {
+    name: 'payment-service'
+} as any)
+
 export default hemera
